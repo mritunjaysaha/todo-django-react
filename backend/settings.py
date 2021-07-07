5a163544e6742b24bcb39777138834ac763a8c81
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-07 08:29:25 +0530

    allowed cors

--- backend/settings.py
+++ backend/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'rest_framework',
     'todo.apps.TodoConfig'
 ]
